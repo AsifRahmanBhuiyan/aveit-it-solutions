cde37a68602dbd43254d34b4a1ca69f105fbedec
Asif Rahman Bhuiyan <asifrah97@gmail.com>
2025-09-03 17:51:49 +0600

    Hero Background Added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import hero_bg from '../../assets/Hero-bg.jpeg'
 
 const Hero = () => {
     return (
         <div>
-            <div className="hero bg-base-200 min-h-screen">
-                <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="bg-[url(hero_bg)] min-h-screen">
+                <div className="flex-col lg:flex-row-reverse">
                     <img
                         src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
                         className="max-w-sm rounded-lg shadow-2xl"
